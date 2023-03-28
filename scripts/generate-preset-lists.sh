1145293ec484a4f83e4c481ce807e468b86ef6f5
daudix-UFO <ddaudix@gmail.com>
2023-03-28 03:54:19 +0300

    feat: Move commands from workflow to script

---
+++ scripts/generate-preset-lists.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# Generate lists of presets
+
+python3 scripts/update-official.py
+python3 scripts/update-curated.py
+
+# Push
+
+git config --global user.name 'Gradience Team'
+git config --global user.email 'AdwCustomizerTeam@proton.me'
+git add presets.json
+git commit -m "ci: Generate preset list"
+git push
